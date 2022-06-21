15e915bb73b62fa5413b30e3748648c27d5f537c
citrus96 <ruslan.citrus.96@gmail.com>
2022-06-21 12:46:51 +0300

    Перенес код в правильную папку

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,52 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
+func Unpack(text string) (string, error) {
+	var sb strings.Builder
+	for i := 0; i < len(text); i++ {
+		currentRune := rune(text[i])
+		if unicode.IsDigit(currentRune) {
+			return "", errors.New("некорректная строка")
+		}
+		var nextRune rune
+		if i < len(text)-1 {
+			nextRune = rune(text[i+1])
+			if currentRune == '\\' {
+				if unicode.IsDigit(nextRune) || nextRune == '\\' {
+					currentRune = nextRune
+					i++
+					if i < len(text)-1 {
+						nextRune = rune(text[i+1])
+					} else {
+						sb.WriteRune(currentRune)
+						continue
+					}
+				} else {
+					return "", errors.New("некорректная строка")
+				}
+			}
+			if unicode.IsDigit(nextRune) {
+				repeatCount, err := strconv.Atoi(string(nextRune))
+				if err != nil {
+					return "", err
+				}
+				rpt := strings.Repeat(string(currentRune), repeatCount)
+				sb.WriteString(rpt)
+				i++
+			} else {
+				sb.WriteRune(currentRune)
+			}
+		} else {
+			sb.WriteRune(currentRune)
+		}
+	}
+	return sb.String(), nil
 	return "", nil
 }
