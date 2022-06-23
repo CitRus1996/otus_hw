32178a69498cce058f04df29e4400a68ac59de15
citrus96 <ruslan.citrus.96@gmail.com>
2022-06-23 09:55:26 +0300

    Снова переписал полностью

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,56 +9,76 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
+type State int64
+
+const (
+	None State = iota
+	Print
+	DontPrint
+	Screen
+)
+
 func Unpack(text string) (string, error) {
 	var sb strings.Builder
-	if len(text) > 0 {
-		for i := 0; i < len(text); i++ {
-			var prev, curr, next rune
-			if i > 0 {
-				prev = rune(text[i-1])
-			}
-			curr = rune(text[i])
-			if i < len(text)-1 {
-				next = rune(text[i+1])
-			}
-
-			err := handleRune(&sb, prev, curr, next)
-			if err != nil {
-				return "", err
-			}
-		}
-	} else {
+	var state State
+	var prev, curr rune
+	if len(text) == 0 {
 		return "", nil
+	}
+	for i := 0; i < len(text); i++ {
+		curr = rune(text[i])
+
+		if state != None {
+			prev = rune(text[i-1])
+		}
+
+		if curr == '\\' {
+			if state != Screen {
+				state = Screen
+			} else {
+				state = Print
+				sb.WriteRune(curr)
+			}
+			continue
+		}
+
+		if unicode.IsLetter(curr) {
+			state = Print
+			sb.WriteRune(curr)
+			continue
+		}
+
+		if err := handleDigit(&sb, prev, curr, &state); err != nil {
+			return "", err
+		}
 	}
 	return sb.String(), nil
 }
 
-func handleRune(sb *strings.Builder, prev, curr, next rune) error {
-	if prev == 0 {
-		if unicode.IsDigit(curr) {
+func handleDigit(sb *strings.Builder, prev, curr rune, state *State) error {
+	if unicode.IsDigit(curr) {
+		switch *state {
+		case Screen:
+			*state = Print
+			sb.WriteRune(curr)
+		case Print:
+			rptCount, err := strconv.Atoi(string(curr))
+			if err != nil {
+				return ErrInvalidString
+			}
+			if rptCount > 0 {
+				repeated := strings.Repeat(string(prev), rptCount-1)
+				sb.WriteString(repeated)
+			} else {
+				temp := sb.String()
+				cut := temp[:len(temp)-1]
+				sb.Reset()
+				sb.WriteString(cut)
+			}
+			*state = DontPrint
+		case None, DontPrint:
 			return ErrInvalidString
 		}
-	}
-	if unicode.IsDigit(prev) && unicode.IsDigit(curr) {
-		return ErrInvalidString
-	}
-
-	if prev == '\\' {
-		if !unicode.IsDigit(curr) && curr != '\\' {
-			return ErrInvalidString
-		}
-	} else if unicode.IsDigit(curr) || curr == '\\' {
-		return nil
-	}
-	if unicode.IsDigit(next) && next != 0 {
-		rptCount, err := strconv.Atoi(string(next))
-		if err != nil {
-			return ErrInvalidString
-		}
-		repeatedLetters := strings.Repeat(string(curr), rptCount)
-		sb.WriteString(repeatedLetters)
-	} else {
-		sb.WriteRune(curr)
 	}
 	return nil
 }
