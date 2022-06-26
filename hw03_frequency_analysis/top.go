9f3c9ec83da9a53a8b0d26ebca1a0ccbd474e0ff
citrus96 <ruslan.citrus.96@gmail.com>
2022-06-26 17:04:54 +0300

    Предварительное решение

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,51 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"fmt"
+	"regexp"
+	"sort"
+)
+
+type Word struct {
+	text  string
+	count int
+}
+
+var re = regexp.MustCompile(`\s`)
+
+func Top10(text string) []string {
+	top10 := make([]string, 0)
 	// Place your code here.
-	return nil
+	if len(text) == 0 {
+		return top10
+	}
+	words := re.Split(text, -1)
+	dictionary := make(map[string]int)
+	for i := range words {
+		if _, ok := dictionary[words[i]]; ok {
+			dictionary[words[i]]++
+		} else {
+			dictionary[words[i]] = 1
+		}
+	}
+
+	countedWords := make([]Word, len(dictionary))
+	for k, v := range dictionary {
+		if len(k) > 0 {
+			countedWords = append(countedWords, Word{text: k, count: v})
+		}
+	}
+	sort.Slice(countedWords, func(i, j int) bool {
+		if countedWords[i].count > countedWords[j].count {
+			return true
+		} else if countedWords[i].count == countedWords[j].count {
+			return countedWords[i].text < countedWords[j].text
+		}
+		return false
+	})
+	fmt.Println(countedWords)
+	for i := 0; i < 10; i++ {
+		top10 = append(top10, countedWords[i].text)
+	}
+	return top10
 }
