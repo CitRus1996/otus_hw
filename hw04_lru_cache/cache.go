4bf3117d86a6e8b3ffb3a697fb70ff98d0f0dc84
citrus96 <ruslan.citrus.96@gmail.com>
2022-07-11 22:23:54 +0300

    Решение без дополнительной задачи.

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,8 +9,6 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -26,5 +24,44 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+	}
+}
+
+func (l lruCache) Set(key Key, value interface{}) bool {
+	if _, ok := l.items[key]; !ok {
+		l.checkLen()
+		item := l.queue.PushFront(value)
+		l.items[key] = item
+		return false
+	} else {
+		l.items[key].Value = value
+		l.queue.MoveToFront(l.items[key])
+		return true
+	}
+}
+
+func (l lruCache) Get(key Key) (interface{}, bool) {
+	if _, ok := l.items[key]; !ok {
+		return nil, false
+	} else {
+		l.queue.MoveToFront(l.items[key])
+		return l.items[key].Value, true
+	}
+}
+
+func (l lruCache) Clear() {
+	l.items = make(map[Key]*ListItem)
+	l.queue = NewList()
+}
+
+func (l lruCache) checkLen() {
+	if l.queue.Len() > l.capacity {
+		last := l.queue.Back()
+		for k := range l.items {
+			if l.items[k] == last {
+				delete(l.items, k)
+			}
+		}
+		l.queue.Remove(last)
 	}
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,89 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
 	// Place your code here.
+	first *ListItem
+	last  *ListItem
+	len   int
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.len
+}
+
+func (l *list) Front() *ListItem {
+	return l.first
+}
+
+func (l *list) Back() *ListItem {
+	return l.last
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+	if l.Len() == 0 {
+		l.first = item
+		l.last = item
+	} else {
+		item.Next = l.first
+		l.first.Prev = item
+		l.first = item
+	}
+	l.len++
+	return item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  nil,
+	}
+	if l.Len() == 0 {
+		l.first = item
+		l.last = item
+	} else {
+		item.Prev = l.last
+		l.last.Next = item
+		l.last = item
+	}
+	l.len++
+	return item
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == l.first {
+		l.first = i.Next
+		l.first.Prev = nil
+		return
+	}
+	if i == l.last {
+		l.last = i.Prev
+		l.last.Next = nil
+	}
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+	l.len--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil {
+		return
+	}
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+	i.Prev.Next = i.Next
+	i.Next = l.first
+	i.Prev = nil
+	l.first.Prev = i
+	l.first = i
 }
