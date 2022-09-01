948c36263b719acb085fc3eaa35ed0a134f5850a
citrus96 <ruslan.citrus.96@gmail.com>
2022-09-01 17:10:11 +0300

    Предварительное решение.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,6 +11,35 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var wg sync.WaitGroup
+	pool := make([]Bi, len(stages)+1)
+	for i := range pool {
+		pool[i] = make(Bi, 10)
+	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for data := range in {
+			pool[0] <- data
+		}
+
+		close(pool[0])
+	}()
+
+	for i := 0; i < len(stages); i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+
+			for data := range stages[j](pool[j]) {
+				pool[j+1] <- data
+			}
+
+			close(pool[j+1])
+		}(i)
+	}
+
+	wg.Wait()
+	return pool[len(stages)]
 }
