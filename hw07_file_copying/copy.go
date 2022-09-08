ebd99b267ba6e4cc64617bdb239738fb263a6018
citrus96 <ruslan.citrus.96@gmail.com>
2022-09-08 22:15:08 +0300

    Предварительное решение.

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,35 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fi, err := os.Lstat(fromPath)
+	if err != nil {
+		return err
+	}
+	size := fi.Size()
+
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit <= 0 {
+		limit = size
+	}
+
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0755)
+	if err != nil {
+		return err
+	}
+
+	dataReader := io.NewSectionReader(sourceFile, offset, limit+1)
+
+	targetFile, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(targetFile, dataReader)
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,109 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type input struct {
+		name             string
+		sourcePath       string
+		targetPath       string
+		offset           int64
+		limit            int64
+		expectedErr      error
+		expectedFilePath string
+	}
+	cases := []input{
+		{
+			"offset0_limit0",
+			"./testdata/input.txt",
+			"./testdata/out_test1.txt",
+			0,
+			0,
+			nil,
+			"./testdata/out_offset0_limit0.txt",
+		},
+		{
+			"offset0_limit10",
+			"./testdata/input.txt",
+			"./testdata/out_test2.txt",
+			0,
+			10,
+			nil,
+			"./testdata/out_offset0_limit10.txt",
+		},
+		{
+			"offset0_limit1000",
+			"./testdata/input.txt",
+			"./testdata/out_test3.txt",
+			0,
+			1000,
+			nil,
+			"./testdata/out_offset0_limit1000.txt",
+		},
+		{
+			"offset0_limit10000",
+			"./testdata/input.txt",
+			"./testdata/out_test4.txt",
+			0,
+			10000,
+			nil,
+			"./testdata/out_offset0_limit10000.txt",
+		},
+		{
+			"offset100_limit1000",
+			"./testdata/input.txt",
+			"./testdata/out_test5.txt",
+			100,
+			1000,
+			nil,
+			"./testdata/out_offset100_limit1000.txt",
+		},
+		{
+			"offset6000_limit1000",
+			"./testdata/input.txt",
+			"./testdata/out_test6.txt",
+			6000,
+			1000,
+			nil,
+			"./testdata/out_offset6000_limit1000.txt",
+		},
+		//{
+		//	"offset0_limit10",
+		//	"./testdata/input.txt",
+		//	"./testdata/out_test2.txt",
+		//	0,
+		//	10,
+		//	nil,
+		//	"./testdata/out_offset0_limit10.txt",
+		//},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := Copy(
+				testCase.sourcePath,
+				testCase.targetPath,
+				testCase.offset,
+				testCase.limit,
+			)
+			expectedFileData, err := os.ReadFile(testCase.expectedFilePath)
+			if err != nil {
+				t.Fatalf("can't read expected file: %s", err)
+			}
+
+			resultFileData, err := os.ReadFile(testCase.targetPath)
+			if err != nil {
+				t.Fatalf("can't read result file: %s", err)
+			}
+
+			require.ErrorIs(t, err, testCase.expectedErr)
+			require.Equal(t, expectedFileData, resultFileData)
+		})
+
+	}
 }
