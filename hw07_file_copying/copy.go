005ee4c03bfbf09629c1fcb63d88187e41f1d4a6
citrus96 <ruslan.citrus.96@gmail.com>
2022-09-17 15:07:52 +0300

    Прикрутил прогресс бар.

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"io"
+	"math"
 	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -11,8 +14,12 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const (
+	bufsize = 1 << 10
+)
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	fi, err := os.Lstat(fromPath)
+	fi, err := os.Stat(fromPath)
 	if err != nil {
 		return err
 	}
@@ -22,8 +29,12 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 		return ErrOffsetExceedsFileSize
 	}
 
-	if limit <= 0 {
+	if limit < 0 {
 		limit = size
+	}
+
+	if limit == 0 || limit+offset > fi.Size() {
+		limit = fi.Size() - offset
 	}
 
 	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0755)
@@ -32,7 +43,7 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	defer sourceFile.Close()
 
-	dataReader := io.NewSectionReader(sourceFile, offset, limit)
+	dataReader := io.NewSectionReader(sourceFile, offset, limit+1)
 
 	targetFile, err := os.OpenFile(toPath, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
@@ -40,9 +51,19 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	defer targetFile.Close()
 
-	_, err = io.Copy(targetFile, dataReader)
-	if err != nil {
-		return err
+	bar := pb.Full.Start64(limit)
+	bar.SetMaxWidth(100)
+	bar.Set(pb.Bytes, true)
+
+	for i := offset; i < offset+limit; i += bufsize {
+		copylen := int64(math.Min(bufsize, float64(limit)))
+		c, err := io.CopyN(targetFile, dataReader, copylen)
+		bar.Add64(c)
+		if (err != nil && !errors.Is(err, io.EOF)) || c > limit {
+			return err
+		}
 	}
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -19,6 +19,10 @@ func init() {
 
 func main() {
 	flag.Parse()
+	if len(from) == 0 || len(to) == 0 {
+		fmt.Println("empty flag '-from' or '-to' ")
+		return
+	}
 	err := Copy(from, to, offset, limit)
 	if err != nil {
 		fmt.Println(err)
